ef7ed71a1afb39bb8ec2f3e06128207000a8168b
shawn <shawn.xue@otms.cn>
2016-04-18 00:01:23 +0800

    update db script

--- create_sequences.sql
+++ create_sequences.sql
@@ -37,3 +37,16 @@ ALTER TABLE public.t_account_info_id_seq
       CACHE 1;
     ALTER TABLE public.t_request_feeds_id_seq
       OWNER TO jetty;
+
+-- Sequence: public.t_operator_info_id_seq
+
+-- DROP SEQUENCE public.t_operator_info_id_seq;
+
+CREATE SEQUENCE public.t_operator_info_id_seq
+  INCREMENT 1
+  MINVALUE 1
+  MAXVALUE 9223372036854775807
+  START 1
+  CACHE 1;
+ALTER TABLE public.t_operator_info_id_seq
+  OWNER TO jetty;
--- create_tables.sql
+++ create_tables.sql
@@ -4,7 +4,7 @@
 
 CREATE TABLE public.t_account_info
 (
-  id integer NOT NULL DEFAULT nextval('t_account_info_id_seq'::regclass),
+  id bigint NOT NULL DEFAULT nextval('t_account_info_id_seq'::regclass),
   name character varying(20),
   sec_id bigint,
   email character varying(100),
@@ -18,34 +18,56 @@ WITH (
 ALTER TABLE public.t_account_info
   OWNER TO jetty;
 
+
+
   -- Table: public.t_account_sec
 
-  -- DROP TABLE public.t_account_sec;
+-- DROP TABLE public.t_account_sec;
 
-  CREATE TABLE public.t_account_sec
+CREATE TABLE public.t_account_sec
+(
+  id bigint NOT NULL DEFAULT nextval('t_account_sec_id_seq'::regclass),
+  role_id bigint,
+  pwd character varying(100),
+  CONSTRAINT pk_sec PRIMARY KEY (id)
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE public.t_account_sec
+  OWNER TO jetty;
+
+
+  -- Table: public.t_request_feeds
+
+  -- DROP TABLE public.t_request_feeds;
+
+  CREATE TABLE public.t_request_feeds
   (
-    id bigint NOT NULL DEFAULT nextval('t_account_sec_id_seq'::regclass),
-    role_id bigint,
-    pwd character(100),
-    CONSTRAINT pk_sec PRIMARY KEY (id)
+    id bigint NOT NULL DEFAULT nextval('t_request_feeds_id_seq'::regclass),
+    owner bigint,
+    CONSTRAINT pk_request_feeds PRIMARY KEY (id)
   )
   WITH (
     OIDS=FALSE
   );
-  ALTER TABLE public.t_account_sec
+  ALTER TABLE public.t_request_feeds
     OWNER TO jetty;
 
-    -- Table: public.t_requests
+    -- Table: public.t_operator_info
 
-    -- DROP TABLE public.t_requests;
+    -- DROP TABLE public.t_operator_info;
 
-    CREATE TABLE public.t_requests
+    CREATE TABLE public.t_operator_info
     (
-      id bigint,
-      owner bigint
+      id bigint NOT NULL DEFAULT nextval('t_operator_info_id_seq'::regclass),
+      skills character varying(20)[],
+      car_type integer[],
+      rating bigint,
+      CONSTRAINT pk_operator_id PRIMARY KEY (id)
     )
     WITH (
       OIDS=FALSE
     );
-    ALTER TABLE public.t_requests
+    ALTER TABLE public.t_operator_info
       OWNER TO jetty;
